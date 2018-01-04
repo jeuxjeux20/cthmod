9a4c7f448da4bdfcc7fe04a87722643be2538806
jeuxjeux20 <jeuxjeux20@outlook.fr>
2018-01-04 17:20:25 +0100

    update

--- CthMod/src/java/com/dieutourtechonology/cthmod/blocks/CthBlock.java
+++ CthMod/src/java/com/dieutourtechonology/cthmod/blocks/CthBlock.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -31,21 +32,29 @@ public class CthBlock extends BasicBlock {
 			"stop breaking me and go on epicub",
 			"u waste ur time",
 			"can u just go outside and plai ?",
-			"me is pro haxor, u is bad haxor, so gtfo"
+			"me is pro haxor, u is bad haxor, so gtfo",
+			"take this diamond and leave me alone ok"
 	};
 	
 	@Override
 	public boolean removedByPlayer(World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
-		if (rand.nextInt(101) < 10) {
-			if (!world.isRemote) {
+		if (rand.nextInt(101) < 10) { // 10% chance of getting him duh
+			if (!world.isRemote) { // Only client side.
 				player.addChatMessage(new ChatComponentText("ok ok u got me now :'( !"));
 			}
-			player.setHealth(player.getHealth() - 1);
-			return world.setBlockToAir(pos);
+			return world.setBlockToAir(pos); // make the block go away
 		}
 		else {
 			if (!world.isRemote) {
-				player.addChatMessage(new ChatComponentText(nopeMessages[rand.nextInt(5)]));
+				int index = rand.nextInt(nopeMessages.length); // get a random index
+				String msg = nopeMessages[index]; // get the message with the random generated index below
+				player.addChatMessage(new ChatComponentText(msg)); // say the message
+				player.setHealth(player.getHealth() - 1); // DAMAGE HIM è_é
+				if (index == 5) { // if it is equals to "take this diamond OK ?"
+					EntityItem item = new EntityItem(world,pos.getX(),pos.getY() + 3,pos.getZ(), new ItemStack(Items.diamond,1));
+					// Create an item at the block position.
+					world.spawnEntityInWorld(item); // Spawn it !
+				}
 			}
 			return false;
 		}
